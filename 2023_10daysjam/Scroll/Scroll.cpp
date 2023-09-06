878eed5cb9a2683d31f6eda207583966c8e5bfd8
美鈴 武田 <k022g1076@m.neec.ac.jp>
2023-09-06 10:21:07 +0900

    関数の追加？

---
+++ 2023_10daysjam/Scroll/Scroll.cpp
@@ -0,0 +1,21 @@
+#include "Scroll.h"
+
+Scroll::Scroll()
+{
+}
+
+Scroll::~Scroll()
+{
+}
+
+void Scroll::Initialize()
+{
+}
+
+void Scroll::Update()
+{
+}
+
+void Scroll::Draw()
+{
+}
---
+++ 2023_10daysjam/Scroll/Scroll.h
@@ -0,0 +1,67 @@
+﻿#pragma once
+#include "Novice.h"
+#include "Vector2.h"
+#include "DirectXCommon.h"
+
+/// <summary>
+/// スクロールのクラス
+/// </summary>
+class Scroll
+{
+public:
+
+	///関数
+
+	/// <summary>
+	/// コンストクラタ
+	/// </summary>
+	Scroll();
+
+	/// <summary>
+	/// デストラクタ
+	/// </summary>
+	~Scroll();
+
+	/// <summary>
+	/// 初期化
+	/// </summary>
+	void Initialize();
+
+	/// <summary>
+	/// 毎フレーム処理
+	/// </summary>
+	void Update();
+
+	/// <summary>
+	/// 前景描画
+	/// </summary>
+	void Draw();
+
+
+	///getter&setter
+
+	/// <summary>
+	/// スクロールのの場所G
+	/// </summary>
+	Vector2 GetScrollPosition() { return position_; };
+
+	/// <summary>
+	/// スクロールののスピードG
+	/// </summary>
+	Vector2 GetScrollSpeed() { return speed_; };
+
+private:
+
+	/// <summary>
+	/// スクロールの場所
+	/// </summary>
+	Vector2 position_ = { 0.0f,0.0f };
+	
+	/// <summary>
+	/// スクロールのスピード
+	/// </summary>
+	Vector2 speed_ = { 0.0f,0.0f };
+
+	uint32_t image_;
+
+};
