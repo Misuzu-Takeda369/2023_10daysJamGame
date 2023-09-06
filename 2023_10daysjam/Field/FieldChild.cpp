ed349565e6c6b313748a5721c3ad9e2520dc4154
美鈴 武田 <k022g1076@m.neec.ac.jp>
2023-09-06 11:57:31 +0900

    file追加(川口さんからもらったやつ)

---
+++ 2023_10daysjam/Field/FieldChild.cpp
@@ -0,0 +1,52 @@
+﻿#include "FieldChild.h"
+#include <stdlib.h>
+#include <time.h>
+
+FieldChild::~FieldChild()
+{
+}
+
+FieldChild::FieldChild()
+{
+}
+
+void FieldChild::Initialize(Vector2 PlayerPos)
+{
+	srand(int(time(nullptr)));
+	int random = rand() % 10;
+	Vector2 plusRange;
+	plusRange.x = float(rand() % 100 + 1);
+	plusRange.y = float(rand() % 100 + 1);
+	Vector2 sign = {1.0f,1.0f};
+	//posX設定
+	if (random > 4) {
+		sign.x = -1;
+	}
+	pos_.x = PlayerPos.x + (float(spawnDistance_ * sign.x))+float(plusRange.x*sign.x);
+	//posY設定
+	random = rand() % 10;
+	if (random > 4) {
+		sign.y = -1;
+	}
+	pos_.y = PlayerPos.y + (float(spawnDistance_ * sign.y))+float(plusRange.y*sign.y);
+
+}
+
+void FieldChild::Update()
+{
+	secondCount_++;
+	if (secondCount_ >= 60) {
+		waitTime_++;
+		secondCount_ = 0;
+	}
+	if (waitTime_ >= deadTime_) {
+		isArrive_ = false;
+	}
+}
+
+void FieldChild::Draw()
+{
+	if (isArrive_) {
+		Novice::DrawSprite(int(pos_.x), int(pos_.y), texture_, 1, 1, 0, WHITE);
+	}
+}
---
+++ 2023_10daysjam/Field/FieldChild.h
@@ -0,0 +1,26 @@
+﻿#pragma once
+#include "Vector2.h"
+#include <Novice.h>
+#include <cstdint>
+class FieldChild
+{
+public:
+	~FieldChild();
+	FieldChild();
+	void Initialize(Vector2 PlayerPos);
+	void Update();
+	void Draw();
+	bool GetIsArrive() {return isArrive_;}
+	static const int spawnWaitTime_ = 1 * 60; //スポーンするまでの時間
+private:
+	Vector2 pos_;
+	const int spawnDistance_ = 200; //プレイヤーからの半径距離(近すぎるところにスポーンしないように)
+	uint32_t texture_ = Novice::LoadTexture("./Resources/Images/FieldChild.png");
+	
+	int waitTime_ = 0; //待機時間
+	int secondCount_ = 0; //秒カウント用
+	const int deadTime_ = 7; //デスポーンする時間(秒)
+	bool isArrive_ = true;
+
+};
+
